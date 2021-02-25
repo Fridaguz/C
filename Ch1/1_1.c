eccf8bbbe2f1c7c7421bcd90e224476d19c4b85f
Fridaguz <78566450+Fridaguz@users.noreply.github.com>
2021-02-25 11:24:33 -0600

    Delete Ch1 directory

--- Ch1/1_1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h> //include information about standard library
-main() //define a function called main that received no arguments values
-
-{ //statements of main are enclosed in braces
-	printf("hello, world\n"); //main calls library function printf to print this sequence of characters \n represents the newline character
-}
